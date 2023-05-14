3db2e0ca9efdcd01af27420140fc1fcd90612190
fauzandp <fauzan27dp@gmail.com>
2023-05-14 15:08:20 +0700

    Project25 | Selfie Share App create UI with collection view and navigationBarItem

--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    
+    var items = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
     }
 
+    @objc func importPicture() {
+        
+    }
 
 }
 
+
+// MARK: - UICollectionViewController Method
+
+extension ViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageViewCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
